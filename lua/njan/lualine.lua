ceb7860563575304fd8e63097cf1191ff9a1124f
Devadathan M B <devadathanmb@gmail.com>
2023-01-22 00:03:33 +0530

    Update

--- lua/njan/lualine.lua
+++ lua/njan/lualine.lua
@@ -1,16 +1,17 @@
 local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
-	vim.notify("Error setting up lualine")
-	return
+    vim.notify("Error setting up lualine")
+    return
 end
 
 -- Lualine colorscheme
 local colorscheme = require("njan.darkplus-lualine")
 lualine.setup({
-	options = {
-		theme = colorscheme,
-		disabled_filetypes = {
-			"NvimTree",
-		},
-	},
+    options = {
+        theme = colorscheme,
+        section_separators = ' ', component_separators = ' ',
+        disabled_filetypes = {
+            "NvimTree",
+        },
+    },
 })
