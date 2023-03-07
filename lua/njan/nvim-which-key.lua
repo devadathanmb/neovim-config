7eaa42c0456da109610d87b396b2d59fbb28a4f4
Devadathan M B <devadathanmb@gmail.com>
2023-03-07 21:53:18 +0530

    Add bindings for live grep and lazy git

--- lua/njan/nvim-which-key.lua
+++ lua/njan/nvim-which-key.lua
@@ -137,11 +137,34 @@ local which_key_mappings = {
 		},
 		-- Telescope git stuff
 		g = {
-			name = "Telescope git",
-			c = { "<cmd>Telescope git_commits<CR>", "Git commits" },
-			s = { "<cmd>Telescope git_status<CR>", "Git status" },
-			b = { "<cmd>Telescope git_branches<CR>", "Git branches" },
-			S = { "<cmd>Telescope git_stash<CR>", "Git stash" },
+			name = "Git",
+			g = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
+			j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
+			k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
+			l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
+			p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
+			r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
+			R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
+			s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+			u = {
+				"<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
+				"Undo Stage Hunk",
+			},
+			o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
+			b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+			c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
+			d = {
+				"<cmd>Gitsigns diffthis HEAD<cr>",
+				"Diff",
+			},
+		},
+
+        -- Searching
+		s = {
+			name = "Search",
+			l = { "<cmd>Telescope live_grep<cr>", "Live grep" },
+			f = { "<cmd>Telescope find_files<cr>", "Find files" },
+			b = { "<cmd>Telescope buffers<cr>", "Find buffers" },
 		},
 	},
 }
