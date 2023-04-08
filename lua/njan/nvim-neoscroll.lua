832864971b8c8984f3866b088039bc5c1f16ee0d
Devadathan M B <devadathanmb@gmail.com>
2023-04-08 12:16:36 +0530

    neoscroll: Revert back neoscroll binds

--- lua/njan/nvim-neoscroll.lua
+++ lua/njan/nvim-neoscroll.lua
@@ -19,8 +19,8 @@ neoscroll.setup({
 
 local t = {}
 -- Syntax: t[keys] = {function, {function arguments}}
-t["<C-k>"] = { "scroll", { "-vim.wo.scroll", "true", "250" } }
-t["<C-j>"] = { "scroll", { "vim.wo.scroll", "true", "250" } }
+t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "250" } }
+t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "250" } }
 t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "450" } }
 t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "450" } }
 t["<C-y>"] = { "scroll", { "-0.10", "false", "100" } }
