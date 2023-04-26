41437eb0a0930a088de30a4c38398a51ff26afe7
Devadathan M B <devadathanmb@gmail.com>
2023-04-26 21:25:39 +0530

    Fix depricated stuff in nvim-tree

--- lua/njan/nvim-tree.lua
+++ lua/njan/nvim-tree.lua
@@ -3,105 +3,123 @@
 
 local status_ok, nvim_tree = pcall(require, "nvim-tree")
 if not status_ok then
-  return
+	return
 end
 
 local config_status_ok, nvim_tree_config = pcall(require, "nvim-tree.config")
 if not config_status_ok then
-  return
+	return
 end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
-nvim_tree.setup {
-  disable_netrw = true,
-  hijack_netrw = true,
-  hijack_cursor = false,
-  update_cwd = true,
-  hijack_directories = {
-    enable = true,
-    auto_open = true,
-  },
-  diagnostics = {
-    enable = true,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
-  },
-  update_focused_file = {
-    enable = true,
-    update_cwd = true,
-    ignore_list = {},
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 500,
-  },
-  view = {
-    width = 30,
-    hide_root_folder = false,
-    side = "left",
-    number = false,
-    relativenumber = false,
-  },
-  actions = {
-    open_file = {
-      window_picker = {
-        enable = true
-      }
-    }
-  },
-  renderer = {
-    highlight_git = true,
-    root_folder_modifier = ":t",
-    icons = {
-      show = {
-        file = true,
-        folder = true,
-        folder_arrow = true,
-        git = true,
-      },
-      glyphs = {
-        default = "",
-        symlink = "",
-        git = {
-          unstaged = "",
-          staged = "S",
-          unmerged = "",
-          renamed = "➜",
-          deleted = "",
-          untracked = "U",
-          ignored = "◌",
-        },
-        folder = {
-          default = "",
-          open = "",
-          empty = "",
-          empty_open = "",
-          symlink = "",
-        },
-      }
-    }
-  }
-}
+-- Mapping ported
+
+local function on_attach(bufnr)
+	local api = require("nvim-tree.api")
+
+	local function opts(desc)
+		return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+	end
+
+	vim.keymap.set("n", "l", api.node.open.edit, opts("Open"))
+	vim.keymap.set("n", "<CR>", api.node.open.edit, opts("Open"))
+	vim.keymap.set("n", "o", api.node.open.edit, opts("Open"))
+	vim.keymap.set("n", "h", api.node.navigate.parent_close, opts("Close Directory"))
+	vim.keymap.set("n", "v", api.node.open.vertical, opts("Open: Vertical Split"))
+end
+
+nvim_tree.setup({
+  on_attach = on_attach,
+	disable_netrw = true,
+	hijack_netrw = true,
+	hijack_cursor = false,
+	update_cwd = true,
+	hijack_directories = {
+		enable = true,
+		auto_open = true,
+	},
+	diagnostics = {
+		enable = true,
+		icons = {
+			hint = "",
+			info = "",
+			warning = "",
+			error = "",
+		},
+	},
+	update_focused_file = {
+		enable = true,
+		update_cwd = true,
+		ignore_list = {},
+	},
+	git = {
+		enable = true,
+		ignore = true,
+		timeout = 500,
+	},
+	view = {
+		width = 30,
+		hide_root_folder = false,
+		side = "left",
+		number = false,
+		relativenumber = false,
+	},
+	actions = {
+		open_file = {
+			window_picker = {
+				enable = true,
+			},
+		},
+	},
+	renderer = {
+		highlight_git = true,
+		root_folder_modifier = ":t",
+		icons = {
+			show = {
+				file = true,
+				folder = true,
+				folder_arrow = true,
+				git = true,
+			},
+			glyphs = {
+				default = "",
+				symlink = "",
+				git = {
+					unstaged = "",
+					staged = "S",
+					unmerged = "",
+					renamed = "➜",
+					deleted = "",
+					untracked = "U",
+					ignored = "◌",
+				},
+				folder = {
+					default = "",
+					open = "",
+					empty = "",
+					empty_open = "",
+					symlink = "",
+				},
+			},
+		},
+	},
+})
 
 local function open_nvim_tree(data)
-  -- buffer is a directory
-  local directory = vim.fn.isdirectory(data.file) == 1
+	-- buffer is a directory
+	local directory = vim.fn.isdirectory(data.file) == 1
 
-  if not directory then
-    return
-  end
+	if not directory then
+		return
+	end
 
-  -- change to the directory
-  vim.cmd.cd(data.file)
+	-- change to the directory
+	vim.cmd.cd(data.file)
 
-  -- open the tree
-  require("nvim-tree.api").tree.open()
+	-- open the tree
+	require("nvim-tree.api").tree.open()
 end
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
