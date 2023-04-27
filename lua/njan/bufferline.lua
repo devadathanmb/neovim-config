16004bcb896e32c6f4224d67afdd96afafd041fc
Devadathan M B <devadathanmb@gmail.com>
2023-04-27 18:51:28 +0530

    Load bufferline only when there's more than one buffers opened

--- lua/njan/bufferline.lua
+++ lua/njan/bufferline.lua
@@ -1,8 +1,17 @@
 local status_ok, bufferline = pcall(require, "bufferline")
 if not status_ok then
-  vim.notify("Bufferline not loaded")
-  return
+	vim.notify("Bufferline not loaded")
+	return
 end
 
-bufferline.setup({
+-- Load when there's only more than one buffer
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead", "TabEnter" }, {
+	pattern = "*",
+	group = vim.api.nvim_create_augroup("BufferLineLazyLoading", {}),
+	callback = function()
+		local count = #vim.fn.getbufinfo({ buflisted = 1 })
+		if count >= 2 then
+			bufferline.setup({})
+		end
+	end,
 })
