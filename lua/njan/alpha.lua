502b207b40a72b6eea162bd22fb973dfc6ff2b64
Devadathan M B <devadathanmb@gmail.com>
2023-04-27 15:49:59 +0530

    alpha instead of dashboard

---
+++ lua/njan/alpha.lua
@@ -0,0 +1,40 @@
+local status_ok, alpha = pcall(require, "alpha")
+if not status_ok then
+	vim.notify("Error while loading alpha")
+	return
+end
+local dashboard = require("alpha.themes.dashboard")
+
+-- Set header
+dashboard.section.header.val = {
+	"                                                     ",
+	"  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
+	"  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
+	"  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
+	"  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
+	"  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
+	"  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
+	"                                                     ",
+}
+
+-- Set menu
+dashboard.section.buttons.val = {
+	dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+	dashboard.button("f", "  > Find file", ":cd $HOME | Telescope find_files<CR>"),
+	dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+	dashboard.button("c", "  > Open NVIM config", ":e ~/.config/nvim <CR>"),
+	dashboard.button("d", "  > Open dotfiles", ":e ~/.config/ <CR>"),
+	dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+}
+
+-- Set footer
+local fortune = require("alpha.fortune")
+dashboard.section.footer.val = fortune()
+
+-- Send config to alpha
+alpha.setup(dashboard.opts)
+
+-- Disable folding on alpha buffer
+vim.cmd([[
+    autocmd FileType alpha setlocal nofoldenable
+]])
