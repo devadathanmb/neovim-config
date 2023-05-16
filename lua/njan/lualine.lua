717736ca558ff6d0bc98bb74f13b6bd3c5b5b71c
Devadathan M B <devadathanmb@gmail.com>
2023-05-16 23:26:18 +0530

    modify lualine theme

--- lua/njan/lualine.lua
+++ lua/njan/lualine.lua
@@ -1,17 +1,82 @@
 local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
-    vim.notify("Error setting up lualine")
-    return
+	vim.notify("Error setting up lualine")
+	return
 end
 
--- Lualine colorscheme
+local hide_in_width = function()
+	return vim.fn.winwidth(0) > 80
+end
+
+local diagnostics = {
+	"diagnostics",
+	sources = { "nvim_diagnostic" },
+	sections = { "error", "warn" },
+	symbols = { error = " ", warn = " " },
+	colored = false,
+	update_in_insert = false,
+	always_visible = true,
+}
+
+local diff = {
+	"diff",
+	colored = false,
+	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+	cond = hide_in_width,
+}
+
+local mode = {
+	"mode",
+	fmt = function(str)
+		return "-" .. str .. "-"
+	end,
+}
+
+local filetype = {
+	"filetype",
+	icons_enabled = false,
+	icon = nil,
+}
+
+local branch = {
+	"branch",
+	icons_enabled = true,
+	icon = "",
+}
+
+local location = {
+	"location",
+	padding = 0,
+}
+
 lualine.setup({
-    options = {
-        theme = "vscode",
-        section_separators = ' ', component_separators = ' ',
-        disabled_filetypes = {
-            "NvimTree",
-            "alpha",
-        },
-    },
+	options = {
+		icons_enabled = true,
+		theme = "vscode",
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = {
+			statusline = { "alpha", "dashboard" },
+		},
+		always_divide_middle = true,
+		globalstatus = true,
+	},
+	sections = {
+		lualine_a = { branch, diagnostics },
+		lualine_b = {},
+		lualine_c = {
+			{ "filename", file_status = true, path = 3 },
+		},
+		lualine_x = { diff, filetype },
+		lualine_y = { "require'lsp-status'.status()" },
+	},
+	inactive_sections = {
+		lualine_a = {},
+		lualine_b = {},
+		lualine_x = { "location" },
+		lualine_y = {},
+		lualine_z = {},
+	},
+	tabline = {},
+	extensions = {},
 })
