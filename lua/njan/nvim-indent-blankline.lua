ab59b47ac5f532e4ecf308795a3e6772d4988ff6
Devadathan M B <devadathanmb@gmail.com>
2023-05-18 00:30:37 +0530

    modify indent blankline configs, change blank line symbol

--- lua/njan/nvim-indent-blankline.lua
+++ lua/njan/nvim-indent-blankline.lua
@@ -4,23 +4,24 @@ if not status_ok then
 	return
 end
 
--- vim.cmd [[highlight IndentBlanklineIndent1 guifg=#E06C75 gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent2 guifg=#E5C07B gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent3 guifg=#98C379 gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent4 guifg=#56B6C2 gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent5 guifg=#61AFEF gui=nocombine]]
--- vim.cmd [[highlight IndentBlanklineIndent6 guifg=#C678DD gui=nocombine]]
-
 vim.opt.list = true
 
-indent_blankline.setup ({
-    show_current_context = true
-    -- char_highlight_list = {
-    --     "IndentBlanklineIndent1",
-    --     "IndentBlanklineIndent2",
-    --     "IndentBlanklineIndent3",
-    --     "IndentBlanklineIndent4",
-    --     "IndentBlanklineIndent5",
-    --     "IndentBlanklineIndent6",
-    -- },
+indent_blankline.setup({
+	show_current_context = true,
+	use_treesitter = true,
+	show_trailing_blankline_indent = false,
+	show_first_indent_level = true,
+	char = "▏",
+	context_char = "▏",
+	buftype_exclude = { "terminal", "nofile" },
+	filetype_exclude = {
+		"help",
+		"startify",
+		"dashboard",
+		"lazy",
+		"neogitstatus",
+		"NvimTree",
+		"Trouble",
+		"text",
+	},
 })
