fab74deebbb5d3020828b88083f91a8cd403e58c
Devadathan M B <devadathanmb@gmail.com>
2023-05-18 00:26:33 +0530

    update barbecue nvim configs: Show modification symbol, get rid off file path ..

--- lua/njan/barbecue.lua
+++ lua/njan/barbecue.lua
@@ -1,7 +1,37 @@
 local status_ok, barbecue = pcall(require, "barbecue")
 if not status_ok then
-    vim.notify("Barbecue not installed or error loading barbecue")
-    return
+	vim.notify("Barbecue not installed or error loading barbecue")
+	return
 end
 
-barbecue.setup()
+barbecue.setup({
+	exclude_filetypes = { "netrw", "toggleterm" },
+	show_dirname = false,
+	show_basename = true,
+	show_modified = true,
+
+	---Whether context text should follow its icon's color.
+	---
+	---@type boolean
+	context_follow_icon_color = false,
+
+	symbols = {
+		---Modification indicator.
+		---
+		---@type string
+		modified = " ",
+
+		---Truncation indicator.
+		---
+		---@type string
+		ellipsis = "…",
+
+		---Entry separator.
+		---
+		---@type string
+		separator = "",
+	},
+	theme = {
+		modified = { fg = "#808080" },
+	},
+})
