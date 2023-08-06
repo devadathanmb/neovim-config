2381f6554b5d78bc0d4788a4fb790fd1cd02d2a7
Devadathan M B <devadathanmb@gmail.com>
2023-08-06 13:20:15 +0530

    Remove nvim-notify from noice

--- lua/user/plugins/noice.lua
+++ lua/user/plugins/noice.lua
@@ -1,52 +1,90 @@
 local M = {
-	"folke/noice.nvim",
-	event = "VeryLazy",
-	dependencies = {
-		-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-		"MunifTanjim/nui.nvim",
-		-- OPTIONAL:
-		--   `nvim-notify` is only needed, if you want to use the notification view.
-		--   If not available, we use `mini` as the fallback
-		"rcarriga/nvim-notify",
-	},
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+  },
+  enabled = true,
 }
 
 function M.config()
-	local noice = require("noice")
-	noice.setup({
-		lsp = {
-			-- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-			override = {
-				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-				["vim.lsp.util.stylize_markdown"] = true,
-				["cmp.entry.get_documentation"] = true,
-			},
-		},
-		messages = {
-			-- NOTE: If you enable messages, then the cmdline is enabled automatically.
-			-- This is a current Neovim limitation.
-			enabled = true, -- enables the Noice messages UI
-			view = "mini", -- default view for messages
-			view_error = "mini", -- view for errors
-			view_warn = "notify", -- view for warnings
-			view_history = "notify", -- view for :messages
-			view_search = "notify", -- view for search count messages. Set to `false` to disable
-		},
-	})
-	require("notify").setup({
-		timeout = 100,
-	})
-	require("lualine").setup({
-		sections = {
-			lualine_x = {
-				{
-					require("noice").api.statusline.mode.get,
-					cond = require("noice").api.statusline.mode.has,
-					color = { fg = "#ff9e64" },
-				},
-			},
-		},
-	})
+  require("noice").setup({
+    lsp = {
+      signature = {
+        enabled = false,
+      },
+      hover = {
+        enabled = false,
+      },
+      progress = {
+        enabled = true,
+      },
+      -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+      override = {
+        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+        ["vim.lsp.util.stylize_markdown"] = true,
+        ["cmp.entry.get_documentation"] = true,
+      },
+    },
+    -- you can enable a preset for easier configuration
+    presets = {
+      bottom_search = true,         -- use a classic bottom cmdline for search
+      command_palette = true,       -- position the cmdline and popupmenu together
+      long_message_to_split = true, -- long messages will be sent to a split
+      inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+      lsp_doc_border = false,       -- add a border to hover docs and signature help
+    },
+    -- stylua: ignore
+    keys = {
+      {
+        "<S-Enter>",
+        function() require("noice").redirect(vim.fn.getcmdline()) end,
+        mode = "c",
+        desc =
+        "Redirect Cmdline"
+      },
+      {
+        "<leader>snl",
+        function() require("noice").cmd("last") end,
+        desc =
+        "Noice Last Message"
+      },
+      {
+        "<leader>snh",
+        function() require("noice").cmd("history") end,
+        desc =
+        "Noice History"
+      },
+      { "<leader>sna", function() require("noice").cmd("all") end, desc = "Noice All" },
+      {
+        "<c-f>",
+        function() if not require("noice.lsp").scroll(4) then return "<c-f>" end end,
+        silent = true,
+        expr = true,
+        desc =
+        "Scroll forward"
+      },
+      {
+        "<c-b>",
+        function() if not require("noice.lsp").scroll(-4) then return "<c-b>" end end,
+        silent = true,
+        expr = true,
+        desc =
+        "Scroll backward"
+      },
+    },
+  })
+  require("lualine").setup({
+    sections = {
+      lualine_x = {
+        {
+          require("noice").api.statusline.mode.get,
+          cond = require("noice").api.statusline.mode.has,
+          color = { fg = "#ff9e64" },
+        }
+      },
+    },
+  })
 end
 
 return M
