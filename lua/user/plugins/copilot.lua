da1880c60dd385ead9ad3909d2bed25e30feeb44
Devadathan M B <devadathanmb@gmail.com>
2023-08-07 09:09:07 +0530

    update copilot configs

--- lua/user/plugins/copilot.lua
+++ lua/user/plugins/copilot.lua
@@ -1,28 +1,27 @@
 local M = {
 	"zbirenbaum/copilot.lua",
+	event = "InsertEnter",
 }
 
 function M.config()
 	local copilot = require("copilot")
 	copilot.setup({
-		panel = {
-			enabled = true,
-			auto_refresh = false,
-			keymap = {
-				jump_prev = "[[",
-				jump_next = "]]",
-				accept = "<CR>",
-				refresh = "gr",
-				open = "<M-CR>",
-			},
-			layout = {
-				position = "bottom", -- | top | left | right
-				ratio = 0.4,
-			},
+		filetypes = {
+			markdown = false, -- overrides default
+			java = false,
+			c = false,
+			sh = function()
+				if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.env.*") then
+					-- disable for .env files
+					return false
+				end
+				return true
+			end,
+			["*"] = false, -- disable for all other filetypes and ignore default `filetypes`
 		},
 		suggestion = {
 			enabled = true,
-			auto_trigger = false,
+			auto_trigger = true,
 			debounce = 75,
 			keymap = {
 				accept = "<M-l>",
@@ -33,19 +32,6 @@ function M.config()
 				dismiss = "<C-]>",
 			},
 		},
-		filetypes = {
-			yaml = false,
-			markdown = false,
-			help = false,
-			gitcommit = false,
-			gitrebase = false,
-			hgcommit = false,
-			svn = false,
-			cvs = false,
-			["."] = false,
-		},
-		copilot_node_command = "node", -- Node.js version must be > 16.x
-		server_opts_overrides = {},
 	})
 end
 
