75eca5fe4c2035a3371857fe77f9fa571d974ce9
Devadathan M B <devadathanmb@gmail.com>
2023-09-03 23:13:54 +0530

    update colorizer config

--- lua/user/plugins/colorizer.lua
+++ lua/user/plugins/colorizer.lua
@@ -9,11 +9,11 @@ function M.config()
 		user_default_options = {
 			RGB = true, -- #RGB hex codes
 			RRGGBB = true, -- #RRGGBB hex codes
-			names = false, -- "Name" codes like Blue or blue
+			names = true, -- "Name" codes like Blue or blue
 			RRGGBBAA = false, -- #RRGGBBAA hex codes
 			AARRGGBB = false, -- 0xAARRGGBB hex codes
-			rgb_fn = false, -- CSS rgb() and rgba() functions
-			hsl_fn = false, -- CSS hsl() and hsla() functions
+			rgb_fn = true, -- CSS rgb() and rgba() functions
+			hsl_fn = true, -- CSS hsl() and hsla() functions
 			css = true, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
 			css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
 			-- Available modes for `mode`: foreground, background,  virtualtext
