23232c7875881bc8109584ec5f44a0a7fc963ae4
Devadathan M B <devadathanmb@gmail.com>
2023-12-02 13:24:26 +0530

    updates

--- lua/user/plugins/treesitter.lua
+++ lua/user/plugins/treesitter.lua
@@ -1,34 +1,35 @@
 local M = {
-  "nvim-treesitter/nvim-treesitter",
-  event = "BufReadPost",
-  dependencies = {
-    {
-      "JoosepAlviste/nvim-ts-context-commentstring",
-      "nvim-tree/nvim-web-devicons",
-    },
-  },
+	"nvim-treesitter/nvim-treesitter",
+	event = "BufReadPost",
+	dependencies = {
+		{
+			"JoosepAlviste/nvim-ts-context-commentstring",
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
 }
 
 function M.config()
-  local configs = require("nvim-treesitter.configs")
-  configs.setup({
-    ensure_installed = "all",
-    sync_install = false,
-    autotag = {
-      enable = true,
-    },
-    ignore_install = { "gomod" }, -- List of parsers to ignore installing
-    highlight = {
-      enable = true,           -- false will disable the whole extension
-      disable = { "" },        -- list of language that will be disabled
-      additional_vim_regex_highlighting = true,
-    },
-    indent = { enable = true, disable = { "yaml" } },
-    context_commentstring = {
-      enable = true,
-      enable_autocmd = false,
-    },
-  })
+	local configs = require("nvim-treesitter.configs")
+	configs.setup({
+		ensure_installed = "all",
+		sync_install = false,
+		autotag = {
+			enable = true,
+		},
+		ignore_install = { "gomod" }, -- List of parsers to ignore installing
+		highlight = {
+			enable = true, -- false will disable the whole extension
+			disable = { "" }, -- list of language that will be disabled
+			additional_vim_regex_highlighting = true,
+		},
+		indent = { enable = true, disable = { "yaml" } },
+		context_commentstring_module = true,
+		--[[ context_commentstring = { ]]
+		--[[   enable = true, ]]
+		--[[   enable_autocmd = false, ]]
+		--[[ }, ]]
+	})
 end
 
 return M
