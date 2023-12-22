3495abc9c5b0e9ca30e6f48d99c72c634c07883d
Devadathan M B <devadathanmb@gmail.com>
2023-12-22 21:48:04 +0530

    add gruvbox colorscheme

---
+++ lua/user/plugins/gruvbox.lua
@@ -0,0 +1,44 @@
+local M = {
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000,
+  enabled = true,
+}
+
+function M.config()
+  -- Default options:
+  require("gruvbox").setup({
+    terminal_colors = true, -- add neovim terminal colors
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = {
+      strings = true,
+      emphasis = true,
+      comments = true,
+      operators = false,
+      folds = true,
+    },
+    strikethrough = true,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    invert_intend_guides = false,
+    inverse = true, -- invert background for search, diffs, statuslines and errors
+    contrast = "", -- can be "hard", "soft" or empty string
+    palette_overrides = {
+      bright_blue = "#3abdb0",
+      bright_purple = "#e37f9a",
+      bright_aqua = "#80d162",
+
+      dark_blue = "#3abdb0",
+      dark_purple = "#e37f9a",
+      dark_aqua = "#80d162",
+    },
+    overrides = {},
+    dim_inactive = false,
+    transparent_mode = true,
+  })
+  vim.cmd("colorscheme gruvbox")
+end
+
+return M
---
+++ lua/user/utils/lualine-themes/gruvbox.lua
@@ -0,0 +1,44 @@
+local colors = {
+  pink = "#D3869B",
+  red = "#FB4934",
+  orange = "#FE8019",
+  green = "#98971A",
+  blue = "#076678",
+  violet = "#B16286",
+  purple = "#8F3F71",
+  white = "#FBF1C7",
+  gray = "#1e1e1e", -- 15% lighter than black
+  black = "#1e1e1e",
+}
+
+colors.black = "NONE"
+colors.gray = "NONE"
+
+return {
+  normal = {
+    a = { fg = colors.white, bg = colors.blue },
+    b = { fg = colors.white, bg = colors.gray },
+    c = { fg = colors.white, bg = colors.black },
+  },
+  insert = {
+    a = { fg = colors.white, bg = colors.orange },
+  },
+  visual = {
+    a = { fg = colors.white, bg = colors.purple },
+  },
+  replace = {
+    a = { fg = colors.white, bg = colors.pink },
+  },
+  command = {
+    a = { fg = colors.white, bg = colors.green },
+  },
+  terminal = {
+    a = { fg = colors.white, bg = colors.violet },
+  },
+  pending = {
+    a = { fg = colors.white, bg = colors.red },
+  },
+  inactive = {
+    a = { fg = colors.white, bg = colors.black },
+  },
+}
