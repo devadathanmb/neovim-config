c4115b21897651b72e1c5e54714afb48e2776b0f
Devadathan M B <devadathanmb@gmail.com>
2024-03-02 12:30:33 +0530

    add copilot indicator to lualine

--- lua/user/plugins/lualine.lua
+++ lua/user/plugins/lualine.lua
@@ -1,10 +1,13 @@
 local M = {
   "nvim-lualine/lualine.nvim",
+  dependencies = {
+    "AndreM222/copilot-lualine",
+  },
 }
 
 function M.config()
   local sl_hl = vim.api.nvim_get_hl_by_name("StatusLine", true)
-  vim.api.nvim_set_hl(0, "Copilot", { fg = "#58A6FF", bg = sl_hl.bg })
+  --[[ vim.api.nvim_set_hl(0, "Copilot", { fg = "NONE", bg = "NONE" }) ]]
   local icons = require("user.utils.icons")
   local diff = {
     "diff",
@@ -12,39 +15,41 @@ function M.config()
     symbols = { added = icons.git.LineAdded, modified = icons.git.LineModified, removed = icons.git.LineRemoved }, -- Changes the symbols used by the diff.
   }
 
-  local copilot = function()
-    local buf_clients = vim.lsp.get_active_clients({ bufnr = 0 })
-    if #buf_clients == 0 then
-      return "LSP Inactive"
-    end
-
-    local buf_client_names = {}
-    local copilot_active = false
-
-    for _, client in pairs(buf_clients) do
-      if client.name ~= "null-ls" and client.name ~= "copilot" then
-        table.insert(buf_client_names, client.name)
-      end
-
-      if client.name == "copilot" then
-        copilot_active = true
-      end
-    end
-
-    if copilot_active then
-      return "%#Copilot#" .. icons.git.Octoface .. "%*"
-    end
-    return ""
-  end
+  --[[ local copilot = function() ]]
+  --[[   local buf_clients = vim.lsp.get_active_clients({ bufnr = 0 }) ]]
+  --[[   if #buf_clients == 0 then ]]
+  --[[     return "LSP Inactive" ]]
+  --[[   end ]]
+  --[[]]
+  --[[   local buf_client_names = {} ]]
+  --[[   local copilot_active = false ]]
+  --[[]]
+  --[[   for _, client in pairs(buf_clients) do ]]
+  --[[     if client.name ~= "null-ls" and client.name ~= "copilot" then ]]
+  --[[       table.insert(buf_client_names, client.name) ]]
+  --[[     end ]]
+  --[[]]
+  --[[     if client.name == "copilot" then ]]
+  --[[       copilot_active = true ]]
+  --[[     end ]]
+  --[[   end ]]
+  --[[]]
+  --[[   if copilot_active then ]]
+  --[[     return "%#Copilot#" .. icons.git.Octoface .. "%*" ]]
+  --[[   end ]]
+  --[[   return "" ]]
+  --[[ end ]]
 
   require("lualine").setup({
     options = {
       disabled_filetypes = { "NvimTree", "Lazy", "alpha" },
-      --[[ theme = require("user.utils.lualine-themes.gruvbox"), ]]
+      --[[ theme = require("user.utils.lualine-themes.arctic"), ]]
       -- component_separators = { left = "", right = "" },
-      section_separators = { left = "", right = "" },
-      component_separators = { left = "", right = "" },
+      --[[ section_separators = { left = "", right = "" }, ]]
+      --[[ component_separators = { left = "", right = "" }, ]]
       --[[ section_separators = { left = "", right = "" }, ]]
+      component_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
 
       ignore_focus = { "NvimTree", "neo-tree" },
     },
@@ -57,9 +62,9 @@ function M.config()
       -- lualine_z = { "progress" },
       lualine_a = { "mode" },
       lualine_b = { { "branch", icon = "" } },
-      lualine_c = { diff, copilot },
+      lualine_c = { diff },
       lualine_x = { "diagnostics" },
-      lualine_y = { "filetype" },
+      lualine_y = { "copilot", "filetype" },
       lualine_z = { "progress" },
     },
     extensions = { "quickfix", "man", "fugitive" },
